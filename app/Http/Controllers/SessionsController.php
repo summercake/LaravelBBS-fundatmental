2e06532c4513dfdcc9f06f6925a151fda1a47aae
summercake@qq.com <summercake@qq.com>
2018-05-25 06:07:18 -0500

    User crud done

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -5,9 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Auth;
-
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -21,7 +27,7 @@ class SessionsController extends Controller
         ]);
         if (Auth ::attempt($credentials)) {
             session() -> flash('success', '欢迎回来！');
-            return redirect() -> route('users.show', [Auth ::user()]);
+            return redirect() -> intended(route('users.show', [Auth ::user()]));
         } else {
             session() -> flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect() -> back();
@@ -30,8 +36,8 @@ class SessionsController extends Controller
 
     public function destroy()
     {
-        Auth::logout();
-        session()->flash('success', '您已成功退出！');
+        Auth ::logout();
+        session() -> flash('success', '您已成功退出！');
         return redirect('login');
     }
 }

f5e955ad0cfcd807b761eaf1b161882f6d159bf7
summercake@qq.com <summercake@qq.com>
2018-05-25 06:32:42 -0500

    Add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,31 +4,40 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
 class User extends Authenticatable
 {
     use Notifiable;
     protected $user = 'user';
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
-    public function gravatar($size = '100'){
+    public static function boot()
+    {
+        parent ::boot();
+        static ::creating(function ($user){
+            $user -> activation_token = str_random(30);
+        });
+    }
+
+    public function gravatar($size = '100')
+    {
         $hash = md5(strtolower(trim($this -> attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }

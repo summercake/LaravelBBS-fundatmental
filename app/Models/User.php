a59a7c9f05b2e780e5accbc5341126d45ccec957
summercake@qq.com <summercake@qq.com>
2018-05-25 20:13:41 -0500

    Add user statuses

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,25 @@ class User extends Authenticatable
 
     public function sendPasswordResetNotification($token)
     {
-        $this->notify(new ResetPassword($token));
+        $this -> notify(new ResetPassword($token));
+    }
+
+    public function statuses()
+    {
+        return $this -> hasMany(Status::class);
+    }
+
+    public function show(User $user)
+    {
+        $statuses = $user -> statuses()
+                          -> orderBy('created_at', 'desc')
+                          -> paginate(30);
+        return view('users.show', compact('user', 'statuses'));
+    }
+
+    public function feed()
+    {
+        return $this -> statuses()
+                     -> orderBy('created_at', 'desc');
     }
 }
